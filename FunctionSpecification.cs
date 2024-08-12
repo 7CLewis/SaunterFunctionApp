fd4a8325c5564494201a47c58b7bb6eac82a87b8
Casey Lewis <c.lewis721@outlook.com>
2024-08-12 16:20:21 -0600

    Initial commit

---
+++ FunctionSpecification.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Options;
+using Saunter;
+using Saunter.Serialization;
+using System.Threading.Tasks;
+
+namespace SaunterFunctionApp;
+public class FunctionSpecification
+{
+    private readonly IAsyncApiDocumentProvider provider;
+    private readonly IAsyncApiDocumentSerializer serializer;
+    private readonly AsyncApiOptions options;
+
+    public FunctionSpecification(
+        IAsyncApiDocumentProvider provider, 
+        IAsyncApiDocumentSerializer serializer, 
+        IOptions<AsyncApiOptions> options
+    )
+    {
+        this.provider = provider;
+        this.serializer = serializer;
+        this.options = options.Value;
+    }
+
+    [FunctionName(nameof(AsyncApiSpec))]
+    public async Task<IActionResult> AsyncApiSpec(
+        [HttpTrigger(AuthorizationLevel.Anonymous, Route = "specifications")] HttpRequest req    
+    )
+    {
+        var schema = provider.GetDocument(options, options.AsyncApi);
+
+        return new OkObjectResult(serializer.Serialize(schema));
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Saunter;
+
+[assembly: FunctionsStartup(typeof(SaunterFunctionApp.Startup))]
+namespace SaunterFunctionApp;
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddAsyncApiSchemaGeneration();
+
+        builder.Services.AddOptions<AsyncApiOptions>()
+            .Configure(c =>
+            {
+                c.AssemblyMarkerTypes = new[] { typeof(FunctionTest) };
+            }
+         );
+    }
+}
